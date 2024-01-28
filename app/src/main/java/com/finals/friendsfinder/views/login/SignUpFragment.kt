c730b5eaaff76ec3d76cf1095204610f7b3c1ad4
truongnguyen2505 <truongnd.work@gmail.com>
2024-01-28 10:09:32 +0700

    config and build ui sign up

---
+++ app/src/main/java/com/finals/friendsfinder/views/login/SignUpFragment.kt
@@ -0,0 +1,23 @@
+package com.finals.friendsfinder.views.login
+
+import android.view.LayoutInflater
+import com.finals.friendsfinder.bases.BaseFragment
+import com.finals.friendsfinder.databinding.FragmentSignUpBinding
+class SignUpFragment: BaseFragment<FragmentSignUpBinding>() {
+
+    override fun observeHandle() {
+        super.observeHandle()
+    }
+
+    override fun setupView() {
+        super.setupView()
+    }
+
+    override fun setupEventControl() {
+        super.setupEventControl()
+    }
+
+    override fun getViewBinding(inflater: LayoutInflater): FragmentSignUpBinding {
+        return FragmentSignUpBinding.inflate(inflater)
+    }
+}
